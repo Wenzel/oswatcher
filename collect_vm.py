9021baa6fae7be251dbeb85a14d07d9396e1ebb7
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-05-10 02:18:16 +0300

    able list content of a directory with libguestfs

--- collect_vm.py
+++ collect_vm.py
@@ -21,11 +21,10 @@ import xml.etree.ElementTree as ET
 # 3rd
 from docopt import docopt
 import libvirt
+import guestfs
 
 
 __SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
-MYISO = os.path.join(__SCRIPT_DIR, 'live.iso')
-HOST_SHARED = os.path.join(__SCRIPT_DIR, 'host_shared')
 
 def run(executable, args, working_dir=os.getcwd()):
     # find executable
@@ -53,56 +52,37 @@ def main(args):
     # search vm
     vm_name = args['<vm_name>']
     vm = con.lookupByName(vm_name)
-    # get xml and list devices
-    xml = vm.XMLDesc()
-    root = ET.fromstring(xml)
-    # find cdrom
-    cdrom = root.find("./devices/disk[@device='cdrom']")
-    # -> source = 'myiso'
-    logging.debug('Setting CDROM source to {}'.format(MYISO))
-    try:
-        # set
-        cdrom.find('source').set('file', MYISO)
-    except:
-        # insert
-        ET.SubElement(cdrom, 'source', {'file': MYISO})
-    # boot from cdrom
-    os = root.find('./os')
-    logging.debug('Setting CDROM as main boot device')
-    try:
-        # set
-        os.find('boot').set('dev', 'cdrom')
-    except:
-        # insert
-        ET.SubElement(os, 'boot', {'dev': 'cdrom'})
+    # find qcow path
+    root = ET.fromstring(vm.XMLDesc())
+    disk = root.find("./devices/disk[@type='file'][@device='disk']")
+    if disk is None:
+        print('Cannot find VM main disk !')
+        sys.exit(1)
+    qcow_path = disk.find('source').get('file')
+    print(qcow_path)
+    # init libguestfs
+    g = guestfs.GuestFS(python_return_dict=True)
+    # attach drive
+    g.add_drive_opts(qcow_path, readonly=1)
+    # run libguestfs backend
+    g.launch()
+    # inspect operating system
+    roots = g.inspect_os()
+    if len(roots) == 0:
+        print('No operating system found !')
+        sys.exit(1)
 
-    # set shared host dir
-    logging.debug('Setting shared filesystem')
-    devices = root.find("./devices")
-    filesystem = root.find("./devices/filesystem")
-    target = "host_shared"
-    if filesystem:
-        # delete it
-        devices.remove(filesystem)
-    # insert
-    logging.debug('Inserting new device : filesystem')
-    ET.SubElement(devices, 'filesystem', {})
-    filesystem = root.find("./devices/filesystem")
-    # set attributes
-    logging.debug('Setting filesystem attributes')
-    filesystem.set('type', 'mount')
-    filesystem.set('accessmode', 'passthrough')
-    ET.SubElement(filesystem, 'source', { 'dir' : HOST_SHARED }) 
-    ET.SubElement(filesystem, 'target', { 'dir' : target }) 
+    # we should have one main filesystem
+    root = roots[0]
+    mps = g.inspect_get_mountpoints (root)
+    print(mps)
+    # mount filesystem
+    for mount_point, device in mps.items():
+        g.mount_ro(device, mount_point)
 
-    # update VM
-    logging.debug('Updating VM definition...')
-    with tempfile.NamedTemporaryFile() as tmp:
-        new_xml = ET.tostring(root)
-        tmp.write(new_xml)
-        tmp.flush()
-        args = ['define', tmp.name]
-        run('virsh', args)
-
+    # enumerate
+    enum = g.ls('/tmp')
+    print(enum)
+    
 if __name__ == '__main__':
     main(docopt(__doc__))
