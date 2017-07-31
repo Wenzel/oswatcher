dc1613355d9a43183ec96c9f3f0b9410d572c6e0
Mathieu Tarral <mathieu.tarral@gmail.com>
2017-08-01 00:44:08 +0300

    update run.sh to use neo4j

--- db/run.sh
+++ db/run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+PASSWORD="admin"
+
+# password needs to be changed after neo4j 3.0
+# a limit of 40000 fd opened is recommended
+
 docker run \
-    -d \
-    -p 127.0.0.1:5432:5432 \
-    -e POSTGRES_PASSWORD="secret" \
+    --detach \
+    --publish=7474:7474 --publish=7687:7687 \
+    --env NEO4J_AUTH="neo4j/${PASSWORD}" \
+    --ulimit=nofile=40000:40000 \
     --name oswatcher_db \
-    oswatcher_db
+    neo4j
