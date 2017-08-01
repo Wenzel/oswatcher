fc86f2c61260585e57991e088e5af8e3a8b3f81c
Mathieu Tarral <mathieu.tarral@gmail.com>
2017-08-02 00:41:51 +0300

    add size property

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Inode(GraphObject):
         # root ?
         if not name:
             name = filepath.anchor
-        self.filename = name
+        self.name = name
         if guestfs.is_file(s_filepath):
             # checksums
             self.md5sum = guestfs.checksum('md5', s_filepath)
@@ -17,12 +17,20 @@ class Inode(GraphObject):
             self.sha256sum = guestfs.checksum('sha256', s_filepath)
             self.sha512sum = guestfs.checksum('sha512', s_filepath)
 
+        # l -> if symbolic link, returns info about the link itself
+        stat = guestfs.lstatns(s_filepath)
+        self.size = stat['st_size']
+
+
+
     # properties
-    filename = Property()
+    name = Property()
+    size = Property()
     md5sum = Property()
     sha1sum = Property()
     sha256sum = Property()
     sha512sum = Property()
+
 
     # relationships
     children = RelatedTo("Inode", "has_child")
