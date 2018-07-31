56b1d74a02c74fab78df0251c91a547854a6e9cf
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-08-01 00:47:10 +0300

    add options to import_libvirt script

--- packer-templates/import_libvirt.py
+++ packer-templates/import_libvirt.py
@@ -6,11 +6,12 @@ Usage:
 
 Options:
     -h --help                   Show this screen.
+    -d --debug                  Enable debug output
     -c --connection=<URI>       Specify a libvirt URI [Default: qemu:///session]
+    -p --pool-name=<POOL>       Specify pool name [Default: oswatcher]
+    -n --vm-name=<NAME>         Specify vm name
 """
 
-import os
-import sys
 import logging
 import shutil
 from pathlib import Path
@@ -19,79 +20,96 @@ import xml.etree.ElementTree as tree
 import libvirt
 from docopt import docopt
 
+SCRIPT_DIR = Path(__file__).absolute().parent
 PREFIX = 'osw'
 POOL_DIR_PATH_REL = '../images'
-OSW_POOL_NAME = 'oswatcher'
 PACKER_OUTPUT_DIR = 'output-qemu'
-SNAPSHOT_XML = """
-<domainsnapshot>
-    <name>base</name>
-</domainsnapshot>
-"""
+POOL_PATH = Path(SCRIPT_DIR / POOL_DIR_PATH_REL).resolve()
 
-def prepare_domain_xml(domain_name, osw_image_path):
+
+def prepare_domain_xml(vm_name, osw_image_path):
     with open('template_domain.xml') as templ:
         domain_xml = templ.read()
-        domain_xml = domain_xml.format(domain_name, osw_image_path)
+        domain_xml = domain_xml.format(vm_name, osw_image_path)
         root = tree.fromstring(domain_xml)
         domain_xml = tree.tostring(root).decode()
         return domain_xml
-    return None
 
-def main(args):
-    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
-    qemu_image = args['<disk_image>']
-    uri = args['--connection']
-    con = libvirt.open(uri)
-    script_dir = Path(__file__).parent
-    storage_path = str(Path(script_dir / POOL_DIR_PATH_REL).resolve())
+
+def setup_storage_pool(con, pool_name, pool_path):
     # check for storage pool
     try:
-        storage = con.storagePoolLookupByName(OSW_POOL_NAME)
+        pool = con.storagePoolLookupByName(pool_name)
     except libvirt.libvirtError:
         # build oswatcher pool xml
         path_elem = tree.Element('path')
-        path_elem.text = storage_path
+        path_elem.text = str(pool_path)
         target_elem = tree.Element('target')
         target_elem.append(path_elem)
         name_elem = tree.Element('name')
-        name_elem.text = OSW_POOL_NAME
+        name_elem.text = pool_name
         pool_elem = tree.Element('pool', attrib={'type': 'dir'})
         pool_elem.append(name_elem)
         pool_elem.append(target_elem)
         pool_xml = tree.tostring(pool_elem).decode('utf-8')
-        # define it
-        storage = con.storagePoolDefineXML(pool_xml)
-        storage.setAutostart(True)
-    # create dir
-    os.makedirs(storage_path, exist_ok=True)
-    # make sure storage is running
-    if not storage.isActive():
-        storage.create()
-    # check if domain is already defined
-    image_name = os.path.basename(qemu_image)
-    domain_name = '{}-{}'.format(PREFIX, image_name)
-    try:
-        domain = con.lookupByName(domain_name)
-    except libvirt.libvirtError:
-        # default system qemu
-        qemu_bin_path = shutil.which('qemu-system-x86_64')
-        # move image to oswatcher pool
-        osw_image_path = os.path.join(storage_path, '{}.qcow2'.format(image_name))
-        shutil.move(qemu_image, osw_image_path)
-        domain_xml = prepare_domain_xml(domain_name, osw_image_path)
-        con.defineXML(domain_xml)
-        logging.info('Domain {} defined.'.format(domain_name))
-        domain = con.lookupByName(domain_name)
-        # take base snapshot
-        domain.snapshotCreateXML(SNAPSHOT_XML)
-        # remove output-qemu
-        output_qemu_path = str(Path(script_dir / PACKER_OUTPUT_DIR))
-        shutil.rmtree(output_qemu_path)
-        # refresh storage pool
-        storage.refresh()
+        # define
+        logging.info('Defining storage pool %s', pool_name)
+        pool = con.storagePoolDefineXML(pool_xml)
+        pool.setAutostart(True)
     else:
-        logging.info('Domain {} already defined'.format(domain_name))
+        logging.info('Storage pool %s already existing', pool_name)
+
+    if not pool.isActive():
+        pool.build()
+        pool.create()
+    return pool
+
+def append_qcow(disk_image):
+    if not disk_image.suffix == '.qcow2':
+        return Path(str(disk_image) + '.qcow2')
+    return disk_image
+
+def setup_domain(con, vm_name, pool, disk_image, pool_path):
+    # check if domain is already defined
+    domain_name = '{}-{}'.format(PREFIX, disk_image.stem)
+    if not vm_name:
+        vm_name = domain_name
+    try:
+        domain = con.lookupByName(vm_name)
+    except libvirt.libvirtError:
+        disk_image = append_qcow(disk_image)
+        # move image to oswatcher pool
+        osw_image_path = pool_path / disk_image.name
+        shutil.move(str(disk_image), str(osw_image_path))
+        domain_xml = prepare_domain_xml(vm_name, osw_image_path)
+        con.defineXML(domain_xml)
+        logging.info('Domain %s defined.', vm_name)
+        domain = con.lookupByName(vm_name)
+        # refresh storage pool
+        pool.refresh()
+    else:
+        logging.info('Domain %s already defined', vm_name)
+    return domain
+
+def main(args):
+    debug = args['--debug']
+    log_level = logging.INFO
+    if debug:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level, format='%(message)s')
+
+    disk_image = Path(args['<disk_image>']).absolute()
+    pool_name = args['--pool-name']
+    vm_name = args['--vm-name']
+    uri = args['--connection']
+    con = libvirt.open(uri)
+
+    pool = setup_storage_pool(con, pool_name, POOL_PATH)
+    setup_domain(con, vm_name, pool, disk_image, POOL_PATH)
+    # remove output-qemu
+    logging.info('Removing output-qemu')
+    output_qemu_path = Path(SCRIPT_DIR / PACKER_OUTPUT_DIR)
+    shutil.rmtree(str(output_qemu_path))
 
 
 if __name__ == '__main__':
