e4a56de239bbd2292a8a40ae7c87fb47dc076e82
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-04-16 21:56:26 +0200

    apps: filter inodes by OS using Cypher query

--- apps/checksec_stats.py
+++ apps/checksec_stats.py
@@ -16,7 +16,7 @@ from collections import Counter
 from docopt import docopt
 from py2neo import Graph
 
-from oswatcher.model import OS, Inode
+from oswatcher.model import OS
 
 DB_PASSWORD = "admin"
 
@@ -36,35 +36,39 @@ def main(args):
     init_logger(args['--debug'])
     logging.info('connect to Neo4j DB')
     graph = Graph(password=DB_PASSWORD)
+
     os_name = args['<os>']
-    os = OS.match(graph).where("_.name = '{}'".format(os_name)).first()
+    os =  OS.match(graph).where("_.name = '{}'".format(os_name)).first()
     if os is None:
         logging.info('unable to find OS %s in the database', os_name)
         logging.info('available operating systems:')
         for os in OS.match(graph):
             logging.info('⭢ %s', os.name)
         return 1
-    inode_checksec_list = Inode.match(graph).where(checksec=True)
+
+    # TODO translate to py2neo API
+    checksec_inodes = graph.run("MATCH (os:OS)-[:OWNS_FILESYSTEM]->(root:Inode)-[:HAS_CHILD*]->(i:Inode) WHERE os.name = 'ubuntu16.04' AND i.checksec = True return i")
     c = Counter()
-    for inode in inode_checksec_list:
-        logging.debug('%s: %s', inode.name, inode.mime_type)
+    for node in checksec_inodes:
+        inode = node['i']
+        logging.debug('%s: %s', inode['name'], inode['mime_type'])
         c['total'] += 1
-        if inode.relro:
+        if inode['relro']:
             c['relro'] += 1
-        if inode.canary:
+        if inode['canary']:
             c['canary'] += 1
-        if inode.nx:
+        if inode['nx']:
             c['nx'] += 1
-        if inode.rpath:
+        if inode['rpath']:
             c['rpath'] += 1
-        if inode.runpath:
+        if inode['runpath']:
             c['runpath'] += 1
-        if inode.symtables:
+        if inode['symtables']:
             c['symtables'] += 1
-        if inode.fortify_source:
+        if inode['fortify_source']:
             c['fortify_source'] += 1
 
-    logging.info('Results for %s', os_name)
+    logging.info('Results for %s', os.name)
     logging.info('Total binaries: %d', c['total'])
     for feature in ['relro', 'canary', 'nx', 'rpath', 'runpath', 'symtables', 'fortify_source']:
         logging.info('%s: %.1f%%', feature, c[feature] * 100 / c['total'])
