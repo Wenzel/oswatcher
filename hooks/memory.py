10f229525d087c4d56234015cf9f024d67736e9e
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-01-13 02:59:05 +0100

    memory: rewrite MemoryDumpHook with Volatility3 API

--- hooks/memory.py
+++ hooks/memory.py
@@ -1,31 +1,96 @@
 # sys
-import logging
 import os
 import stat
+import datetime
+from pathlib import Path
+from typing import Any, List, Tuple, Dict, Optional
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 # 3rd
 import libvirt
 from see import Hook
-# silence rekall output
-logger = logging.getLogger()
-log_level = logger.getEffectiveLevel()
-logger.setLevel(logging.CRITICAL)
-from rekall import session
-# restore our logging level
-logger.setLevel(log_level)
+import volatility.plugins
+from volatility import framework
+from volatility.framework import contexts, automagic, interfaces
+from volatility.framework.interfaces.renderers import RenderOption
+from volatility.framework.renderers import format_hints
+from volatility.cli.text_renderer import quoted_optional, hex_bytes_as_text, display_disassembly
+
+
+BASE_CONFIG_PATH = 'plugins'
+
+
+# customizing Volatility's JSON renderers
+# this code is based on volatility.cli.text_renderer.JSONRenderer
+# but the upstream class would always print the result on sys.stdout.
+# here, we simply store the result in self.result, and return the data
+# when get_result() is called
+
+# storing this function in hooks.memory for now,
+# may be removed when Volatility decided to implement a way
+# to get the data in the upstream class
+class JsonRenderer(interfaces.renderers.Renderer):
+    _type_renderers = {
+        format_hints.HexBytes: quoted_optional(hex_bytes_as_text),
+        interfaces.renderers.Disassembly: quoted_optional(display_disassembly),
+        datetime.datetime: lambda x: x.isoformat() if not isinstance(x, interfaces.renderers.BaseAbsentValue) else None,
+        'default': lambda x: x
+    }
+
+    name = 'JSON'
+    structured_output = True
+
+    def __init__(self, options: Optional[List[RenderOption]] = None):
+        self.result = None
+        super().__init__(options)
+
+    def get_render_options(self) -> List[RenderOption]:
+        pass
+
+    def get_result(self):
+        """Outputs the JSON data to a file in a particular format"""
+        return self.result
+
+    def render(self, grid: interfaces.renderers.TreeGrid):
+        final_output = ({}, [])
+
+        def visitor(
+                node: Optional[interfaces.renderers.TreeNode],
+                accumulator: Tuple[Dict[str, Dict[str, Any]], List[Dict[str, Any]]],
+        ) -> Tuple[Dict[str, Dict[str, Any]], List[Dict[str, Any]]]:
+            # Nodes always have a path value, giving them a path_depth of at least 1, we use max just in case
+            acc_map, final_tree = accumulator
+            node_dict = {'__children': []}
+            for column_index in range(len(grid.columns)):
+                column = grid.columns[column_index]
+                renderer = self._type_renderers.get(column.type, self._type_renderers['default'])
+                data = renderer(list(node.values)[column_index])
+                if isinstance(data, interfaces.renderers.BaseAbsentValue):
+                    data = None
+                node_dict[column.name] = data
+            if node.parent:
+                acc_map[node.parent.path]['__children'].append(node_dict)
+            else:
+                final_tree.append(node_dict)
+            acc_map[node.path] = node_dict
+
+            return acc_map, final_tree
+
+        if not grid.populated:
+            grid.populate(visitor, final_output)
+        else:
+            grid.visit(node=None, function=visitor, initial_accumulator=final_output)
+
+        self.result = final_output[1]
 
 
 class MemoryDumpHook(Hook):
 
     def __init__(self, parameters):
         super().__init__(parameters)
-        debug = self.configuration.get('debug', False)
-        if not debug:
-            # silence rekall output
-            logging.getLogger('rekall.1').setLevel(logging.CRITICAL)
+        self.debug = self.configuration.get('debug', False)
         self.context.subscribe('desktop_ready', self.dump_memory)
-        self.context.subscribe('memory_dumped', self.prepare_rekall_session)
+        self.context.subscribe('memory_dumped', self.prepare_forensic_session)
 
     def dump_memory(self, event):
         # take temporary memory dump
@@ -49,15 +114,16 @@ class MemoryDumpHook(Hook):
                 # trigger event
                 self.context.trigger('memory_dumped', memdump_path=ram_dump.name)
 
-    def prepare_rekall_session(self, event):
+    def prepare_forensic_session(self, event):
         memdump_path = event.memdump_path
-        s = session.Session(
-            filename=memdump_path,
-            autodetect=["rsds"],
-            autodetect_build_local='none',
-            format='data',
-            profile_path=[
-                "http://profiles.rekall-forensic.com"
-            ]
-        )
-        self.context.trigger('rekall_session', session=s)
+        # init Volatility3
+        failures = framework.import_files(volatility.plugins, True)
+        if failures and self.debug:
+            for f in failures:
+                self.logger.debug('Plugin failed to load: %s', f)
+        plugin_list = framework.list_plugins()
+        ctx = contexts.Context()  # Construct a blank context
+        automagics = automagic.available(ctx)  # Find all the automagics
+        # populate ctx.config
+        ctx.config['automagic.LayerStacker.single_location'] = Path(memdump_path).as_uri()
+        self.context.trigger('forensic_session', context=ctx, automagics=automagics, plugin_list=plugin_list)
