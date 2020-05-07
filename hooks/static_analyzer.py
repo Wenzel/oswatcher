419c83e6621e89ab7c9b0565c88aa057830b534b
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-05-07 03:15:47 +0200

    static_analyzer: rename camelcase variables

--- hooks/static_analyzer.py
+++ hooks/static_analyzer.py
@@ -19,19 +19,19 @@ from oswatcher.model import InodeType
 
 @dataclass
 class checkPE:
-    dynamicBase: bool
-    noSEH: bool
-    guardCF: bool
-    forceIntegrity: bool
-    nxCompat: bool
-    highEntropyVA: bool
-    codeSize: str
-    numFunctionsExported: int
-    imageSize: str
-    hasEmbeddedSig: bool
-    hasCatSig: bool
-    catFileName: str
-    importedLibs: list
+    dynamic_base: bool
+    no_seh: bool
+    guard_cf: bool
+    force_integrity: bool
+    nx_compat: bool
+    high_entropy_va: bool
+    code_size: str
+    num_functions_exported: int
+    image_size: str
+    has_embedded_sig: bool
+    has_cat_sig: bool
+    cat_filename: str
+    imported_libs: list
 
 
 class StaticAnalyzerHook(Hook):
@@ -58,18 +58,18 @@ class StaticAnalyzerHook(Hook):
         # subscribe on "filesystem_new_file" events
         self.context.subscribe("filesystem_new_file", self.handle_new_file)
 
-    def formatSize(self, size: int, precision: int = 2) -> str:
+    def format_size(self, size: int, precision: int = 2) -> str:
         suffix = ['B', 'KB', 'MB', 'GB']
-        suffixIndex = 0
+        suffix_index = 0
 
         if size == 0:
             return "0"
         else:
-            while size > 1024 and suffixIndex < 3:
-                suffixIndex += 1
+            while size > 1024 and suffix_index < 3:
+                suffix_index += 1
                 size = size / 1024.0
 
-        return "%.*f%s" % (precision, size, suffix[suffixIndex])
+        return "%.*f%s" % (precision, size, suffix[suffix_index])
 
     def search_cat(self, input_stream: Decoder, sha1_hash: str, sha256_hash: str, spc_indirect_found: int) -> bool:
         while not input_stream.eof():
@@ -82,14 +82,14 @@ class StaticAnalyzerHook(Hook):
                 elif tag.nr == asn1.Numbers.OctetString:
                     if spc_indirect_found == 1:
                         spc_indirect_found = 0
-                        imageHash = value.hex().upper()
-                        if imageHash == sha256_hash or imageHash == sha1_hash:
+                        image_hash = value.hex().upper()
+                        if image_hash == sha256_hash or image_hash == sha1_hash:
                             return True
             elif tag.typ == asn1.Types.Constructed:
                 input_stream.enter()
-                catalogFound = self.search_cat(
+                catalog_found = self.search_cat(
                     input_stream, sha1_hash, sha256_hash, spc_indirect_found)
-                if catalogFound:
+                if catalog_found:
                     input_stream.leave()
                     return True
                 input_stream.leave()
@@ -131,47 +131,47 @@ class StaticAnalyzerHook(Hook):
                 return
 
             # extraction of relevant DLL characteristics
-            dynamicBase = pe.optional_header.has(
+            dynamic_base = pe.optional_header.has(
                 lief.PE.DLL_CHARACTERISTICS.DYNAMIC_BASE)
-            noSEH = pe.optional_header.has(
+            no_seh = pe.optional_header.has(
                 lief.PE.DLL_CHARACTERISTICS.NO_SEH)
-            guardCF = pe.optional_header.has(
+            guard_cf = pe.optional_header.has(
                 lief.PE.DLL_CHARACTERISTICS.GUARD_CF)
-            forceIntegrity = pe.optional_header.has(
+            force_integrity = pe.optional_header.has(
                 lief.PE.DLL_CHARACTERISTICS.FORCE_INTEGRITY)
-            nxCompat = pe.optional_header.has(
+            nx_compat = pe.optional_header.has(
                 lief.PE.DLL_CHARACTERISTICS.NX_COMPAT)
-            highEntropyVA = pe.optional_header.has(
+            high_entropy_va = pe.optional_header.has(
                 lief.PE.DLL_CHARACTERISTICS.HIGH_ENTROPY_VA)
 
             # Authenticode checks (embedded and detached signatures)
-            hasEmbeddedSig = pe.has_signature
+            has_embedded_sig = pe.has_signature
 
             cat_filepath = None
             has_cat_sig = None
             if self.catalogs:
-                if not hasEmbeddedSig:
+                if not has_embedded_sig:
                     with open(local_path, "rb") as pe_file_obj:
                         fingerprinter = AuthenticodeFingerprinter(pe_file_obj)
                         fingerprinter.add_authenticode_hashers(
                             hashlib.sha1, hashlib.sha256)
                         hashes = (fingerprinter.hashes()).get('authentihash')
-                        sha1Hash = hashes.get('sha1').hex().upper()
-                        sha256Hash = hashes.get('sha256').hex().upper()
+                        sha1_hash = hashes.get('sha1').hex().upper()
+                        sha256_hash = hashes.get('sha256').hex().upper()
                         has_cat_sig, cat_filepath = self.has_cat_signature(
                             gfs_wrapper, self.CATROOT_PATH,
-                            sha1Hash, sha256Hash)
+                            sha1_hash, sha256_hash)
 
             # image implementation characteristics
-            codeSize = self.formatSize(pe.optional_header.sizeof_code)
-            imageSize = self.formatSize(pe.optional_header.sizeof_image)
-            numFunctionsExported = len(pe.exported_functions)
-            importedLibs = []
+            code_size = self.format_size(pe.optional_header.sizeof_code)
+            image_size = self.format_size(pe.optional_header.sizeof_image)
+            num_functions_exported = len(pe.exported_functions)
+            imported_libs = []
             for importedLib in pe.imports:
-                importedLibs.append(importedLib.name)
-            check_pe = checkPE(dynamicBase, noSEH, guardCF, forceIntegrity,
-                               nxCompat, highEntropyVA, codeSize,
-                               numFunctionsExported, imageSize,
-                               hasEmbeddedSig, has_cat_sig,
-                               cat_filepath, importedLibs)
+                imported_libs.append(importedLib.name)
+            check_pe = checkPE(dynamic_base, no_seh, guard_cf, force_integrity,
+                               nx_compat, high_entropy_va, code_size,
+                               num_functions_exported, image_size,
+                               has_embedded_sig, has_cat_sig,
+                               cat_filepath, imported_libs)
             self.logger.info(check_pe)
